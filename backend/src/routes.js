381ade78df5c788755ebba8ae4e3d26eff758f95
André Ferraz <andreferraz25384@gmail.com>
2020-03-30 00:35:37 -0300

    adding ONG record validation

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,13 +3,22 @@ const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const routes = express.Router();
 
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
 
 routes.get('/profile', ProfileController.index);
 routes.get('/incidents', IncidentController.index);
